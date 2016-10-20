392cedd34703450d4f521bb123c3e58b03f7b301
Alexis Jugnon <alexisjugnon@gmail.com>
2016-10-20 19:41:31 +0200

    v0.1

---
+++ CaveAVin/DAO/BDD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class BDD
+    {
+        private static BDD instance;
+        private BDD()
+        {
+            con = new MySqlConnection();
+        }
+        public static BDD Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BDD();
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Obtient la connexion à la base de données
+        /// </summary>
+        public IDbConnection Connexion
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+        private IDbConnection con;
+
+    }
+}
