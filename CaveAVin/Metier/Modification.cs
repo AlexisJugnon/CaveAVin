b132a65f28f99bc9eeaf19804daecdda590b9909
Alexis Jugnon <alexisjugnon@gmail.com>
2016-10-23 21:42:36 +0200

    v0.9

---
+++ CaveAVin/Metier/Modification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    class Modification
+    {
+        #region opérations
+        public void Ajouter(Bouteille b) { }
+        public void Ajouter(Bouteilles b) { }
+        public void Supprimer(Bouteille b) { }
+        public Bouteille[] Lister() { return null; }
+        #endregion
+    }
+}
