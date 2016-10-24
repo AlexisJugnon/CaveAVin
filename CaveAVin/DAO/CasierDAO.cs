4e4f3f0f53a2e25fd7ae5f509a9fb6d466e5d353
goncalves <cindy.goncalves@iut-dijon.u-bourgogne.fr>
2016-10-24 17:21:06 +0200

    AppelationDAO et CasierDAO ok

--- CaveAVin/DAO/CasierDAO.cs
+++ CaveAVin/DAO/CasierDAO.cs
@@ -9,9 +9,11 @@ using System.Diagnostics;
 
 namespace DAO
 {
-    public class CasierDAO:Metier.ICasierDAO
+    public class CasierDAO : Metier.ICasierDAO
     {
+
         private IDbConnection con;
+
         /// <summary>
         /// Initialise l'objet DAO
         /// </summary>
@@ -24,36 +26,127 @@ namespace DAO
 
         public Casier Chercher(int ID)
         {
-            throw new NotImplementedException();
+            Casier c = null;
+
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM CASIER WHERE IdCasier=" + ID.ToString();
+                IDataReader reader = com.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    c = reader2Casier(reader);
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return c;
+
         }
 
-        public void Créer(Casier p)
+        public void Créer(Casier c)
         {
-            throw new NotImplementedException();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "INSERT INTO Casier(NomCasier) VALUES('" + c.Nom + "');";
+                com.ExecuteNonQuery();
+                com.CommandText = "SELECT LAST_INSERT_ID() FROM Casier;";
+                IDataReader reader = com.ExecuteReader();
+                int id = 1;
+                if (reader.Read())
+                    id = Convert.ToInt32(reader[0]);
+                c.Id = id;
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         public Casiers Lister()
         {
-            throw new NotImplementedException();
+            Casiers liste = new Casiers();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM Casier";
+                IDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    Casier c = reader2Casier(reader);
+                    liste.Ajouter(c);
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+            return liste;
         }
 
-        public void Relire(Casier p)
+        public void Relire(Casier c)
         {
-            throw new NotImplementedException();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM Casier WHERE IdCasier=" + c.Id.ToString();
+                IDataReader reader = com.ExecuteReader();
+                if (reader.Read())
+                {
+                    c.Nom = reader["NomCasier"].ToString();
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
-        public void Sauver(Casier p)
+        public void Sauver(Casier c)
         {
-            throw new NotImplementedException();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "UPDATE Casier SETNomCasier='" + c.Nom + "' WHERE IdCasier=" + c.Id.ToString();
+                com.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
-        public void Supprimer(Casier p)
+        public void Supprimer(Casier c)
         {
-            throw new NotImplementedException();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "DELETE FROM Casier WHERE IdCasier=" + c.Id.ToString();
+                com.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
+
         private Casier reader2Casier(IDataReader reader)
         {
-            return null;
+            Casier c = new Casier();
+            c.Id = Convert.ToInt32(reader["IdCasier"]);
+            c.Nom = reader["NomCasier"].ToString();
+            return c;
         }
     }
+
 }
