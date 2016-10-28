1e269350038e01793f5ffa9e23ad31f856b4979b
goncalves <cindy.goncalves@iut-dijon.u-bourgogne.fr>
2016-10-28 20:23:01 +0200

    Contenance DAO, merci de regarder, j'ai eu un bug sur une ligne je l'ai mise en commentaire, je sais pas comment faire ? Merci !

--- CaveAVin/DAO/ContenanceDAO.cs
+++ CaveAVin/DAO/ContenanceDAO.cs
@@ -12,6 +12,7 @@ namespace DAO
     class ContenanceDAO:Metier.IContenanceDAO
     {
         private IDbConnection con;
+
         /// <summary>
         /// Initialise l'objet DAO
         /// </summary>
@@ -24,36 +25,149 @@ namespace DAO
 
         public Contenance Chercher(int ID)
         {
-            throw new NotImplementedException();
+            Contenance c = null;
+
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM CONTENANCE WHERE IdContenance=" + ID.ToString();
+                IDataReader reader = com.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    c = reader2Contenance(reader);
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return c;
+
         }
 
-        public void Créer(Contenance p)
+        public void Créer(Contenance c)
         {
-            throw new NotImplementedException();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "INSERT INTO Contenance(valeur) VALUES('" + c.Valeur + "');";
+                com.ExecuteNonQuery();
+                com.CommandText = "SELECT LAST_INSERT_ID() FROM Contenance;";
+                IDataReader reader = com.ExecuteReader();
+                int id = 1;
+                if (reader.Read())
+                    id = Convert.ToInt32(reader[0]);
+                c.Id = id;
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
         public Contenances Lister()
         {
-            throw new NotImplementedException();
+            Contenances liste = new Contenances();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM Contenance";
+                IDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    Contenance c = reader2Contenance(reader);
+                    liste.Ajouter(c);
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+            return liste;
         }
 
-        public void Relire(Contenance p)
+        public Contenances Lister(Cave c)
         {
-            throw new NotImplementedException();
+            Contenances liste = new Contenances();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM Contenance WHERE IdContenance=" + c.Id.ToString();
+                IDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    Contenance cd = reader2Contenance(reader);
+                    liste.Ajouter(cd);
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+            return liste;
         }
 
-        public void Sauver(Contenance p)
+        public void Relire(Contenance c)
         {
-            throw new NotImplementedException();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "SELECT * FROM Contenance WHERE IdContenance=" + c.Id.ToString();
+                IDataReader reader = com.ExecuteReader();
+                if (reader.Read())
+                {
+                   // c.Valeur = reader["valeur"].ToString();  //je sais pas pourquoi cette ligne ne fonctionne pas ? help ?
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
         }
 
-        public void Supprimer(Contenance p)
+        public void Sauver(Contenance c)
         {
-            throw new NotImplementedException();
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "UPDATE Contenance SETvaleur='" + c.Valeur + "' WHERE IdContenance=" + c.Id.ToString();
+                com.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Close();
+            }
         }
+
+        public void Supprimer(Contenance c)
+        {
+            con.Open();
+            try
+            {
+                IDbCommand com = con.CreateCommand();
+                com.CommandText = "DELETE FROM Contenance WHERE IdContenance=" + c.Id.ToString();
+                com.ExecuteNonQuery();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
         private Contenance reader2Contenance(IDataReader reader)
         {
-            return null;
+            Contenance c = new Contenance();
+            c.Id = Convert.ToInt32(reader["IdContenance"]);
+            //c.Valeur = reader["valeur"].ToString(); //je sais pas pourquoi cette ligne ne fonctionne pas ? help ?
+            return c;
         }
     }
+
 }
